ad683912ab6556991308b2391eaba2705076bf27
itggot-louise-claesson <louise.claesson@itggot.se>
2018-05-18 13:59:39 +0200

    La till popupmenu + textarea

---
+++ site/js/menu.js
@@ -0,0 +1,20 @@
+
+    function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    } 
+
+    function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "#ffffff";
+    }
+
+    function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "#ffffff";
+    } 
