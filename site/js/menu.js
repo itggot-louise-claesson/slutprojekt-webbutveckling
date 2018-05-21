1fc5c4ab55034d2e3e781b17495b67ff855fdb90
itggot-louise-claesson <louise.claesson@itggot.se>
2018-05-21 20:29:32 +0200

    Sida för mobilversion klar + header fixad

--- site/js/menu.js
+++ site/js/menu.js
@@ -1,11 +1,11 @@
 
-    function openNav() {
+   /* function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     }
 
     function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    } 
+    } */
 
     function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
