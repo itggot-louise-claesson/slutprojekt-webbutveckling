0f69f23ca19b11e50e6710ff7f868501c333ede3
itggot-louise-claesson <louise.claesson@itggot.se>
2018-05-25 22:55:33 +0200

    lagt in alla sista grejer - sidan är klar!

--- site/public/js/menu.js
+++ site/public/js/menu.js
@@ -1,12 +1,3 @@
-
-   /* function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    }
-
-    function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    } */
-
     function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "0";
